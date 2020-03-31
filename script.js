4b812a26dd836ee5fa5f5ae48867df326f9b7026
kn <huyhoang8398@gmail.com>
2020-04-01 05:50:35 +0700

    confirm property

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var todoItems = [];
 
 class Item {
-  constructor(data) {
+  constructor(data, confirm) {
     this.data = data;
+    this.confirm = confirm;
   }
 }
 
@@ -22,10 +23,11 @@ function getData() {
 
 function newItem() {
   let data = document.getElementById("new-todo").value;
-  if(!data) {
+  let confirm = false;
+  if (!data) {
     alert("You should add todo item!");
   } else {
-    let item = new Item(data);
+    let item = new Item(data, confirm);
     todoItems.push(item);
     saveData();
     document.getElementById("new-todo").value = "";
@@ -37,17 +39,33 @@ function listItems() {
   let li = "";
   for (let data in todoItems) {
     let todo = todoItems[data].data;
-    console.log(todo);
-    li += `<li class="list-group-item list-group-item-action list-group-item-info" id="data${data}">${todo}</br></br><button class="nes-btn is-warning mr-5" onclick="delItem(this.id)" id="delbut${data}">Done</button><label><input type="checkbox" class="nes-checkbox" /><span>Check</span></label></li>`;
+    let confirm = todoItems[data].confirm;
+    if (confirm) {
+      li += `<li class="list-group-item list-group-item-action list-group-item-info" id="data${data}">${todo}</br></br><button class="nes-btn is-warning mr-5" onclick="delItem(this.id)" id="delbut${data}">Done</button><label><input type="checkbox" class="nes-checkbox" id="confirm${data}" conclick="checkedItem(this.id)" checked/><span>Check</span></label></li>`;
+    } else {
+      li += `<li class="list-group-item list-group-item-action list-group-item-info" id="data${data}">${todo}</br></br><button class="nes-btn is-warning mr-5" onclick="delItem(this.id)" id="delbut${data}">Done</button><label><input type="checkbox" class="nes-checkbox" id="confirm${data}" onclick="checkedItem(this.id)" /><span>Check</span></label></li>`;
+    }
   }
   $("#todo-list").html(li);
 }
-
 function delItem(index) {
   let dataDelID = "data" + index.slice(-1);
   let div = document.getElementById(dataDelID);
   div.style.display = 'none';
   todoItems.splice(index.slice(-1), 1);
+  saveData();
+}
+
+function checkedItem(index) {
+  let checkedID = index.slice(-1);
+  let checkboxID = "confirm" + checkedID;
+  let checkBox = document.getElementById(checkboxID);
+  if (checkBox.checked == true) {
+    todoItems[checkedID].confirm = true;
+  }
+  else {
+    todoItems[checkedID].confirm = false;
+  }
   saveData();
 }
 
