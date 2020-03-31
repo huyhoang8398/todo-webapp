fd01200020f3f46ec539a9d025c0104205cc97a5
kn <huyhoang8398@gmail.com>
2020-04-01 03:35:05 +0700

    new jquery version

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ var todoItems = [];
 
 class Item {
   constructor(data) {
-    this._data = data;
+    this.data = data;
   }
-  get data() {
-    return this._data;
-  }
-  set data(updateData) {
-    this._data = updateData;
+}
+
+function saveData() {
+  let str = JSON.stringify(todoItems);
+  localStorage.setItem("todos", str);
+}
+
+function getData() {
+  let str = localStorage.getItem("todos");
+  todoItems = JSON.parse(str);
+  console.log(todoItems);
+  if (!todoItems) {
+    todoItems = [];
   }
 }
 
@@ -16,6 +24,8 @@ function newItem() {
   let data = document.getElementById("new-todo").value;
   let item = new Item(data);
   todoItems.push(item);
+  saveData();
+  document.getElementById("new-todo").value = "";
   listItems();
 }
 
@@ -23,14 +33,19 @@ function listItems() {
   let li = "";
   for (let data in todoItems) {
     let todo = todoItems[data].data;
-    li += `<li class="list-group-item list-group-item-action list-group-item-info">${todo}</li><button class="btn btn-outline-danger btn-sm" onclick="delItem()">Done</button>`;
+    console.log(todo);
+    li += `<li class="list-group-item list-group-item-action list-group-item-info" id="data${data}">${todo}</br></br><div class="btn-group" role="group" aria-label="button-option"><button class="btn btn-outline-danger btn-sm mr-1" onclick="delItem(this.id)" id="delbut${data}">Done</button><div class="btn-group-toggle" data-toggle="buttons"><label class="btn btn-outline-info btn-sm"><input type="checkbox" autocomplete="off">Checked</label></div></div></li>`;
   }
-  console.log(li);
   $("#todo-list").html(li);
 }
 
-function delItem() {
-  let div = this.parentElement;
-  console.log(div);
-  div.style.display = "none";
+function delItem(index) {
+  let dataDelID = "data" + index.slice(-1);
+  let div = document.getElementById(dataDelID);
+  div.style.display = 'none';
+  todoItems.splice(index.slice(-1), 1);
+  saveData();
 }
+
+getData();
+listItems();
